431befc5204020bbb897ca0b13e6592aa3def315
Peter Kim Peetear & Daniela Schiano di Cola <pair+d0ntg0m0ng.daschi@devbootcamp.com>
2015-08-21 12:21:27 -0700

    Add index html and miniquery js defined module with getby id,tag,class functions

---
+++ miniQuery.js
@@ -0,0 +1,19 @@
+var SweetSelector = (function(){
+  var getById = function(elementId){
+    document.getElementById(elementId)
+  }
+
+  var getByClass = function(elementClass){
+    document.getElementsByClassName(elementClass)
+  }
+
+  var getByTag = function(elementTag){
+    document.getElementByTagName(elementTag)
+  }
+
+  var select = function(element){
+
+  }
+
+
+})()
